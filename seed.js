6fb9b9838cba5f5decd31016297a6a923c7a26e3
Nana Adjei Manu <claeusdev@gmail.com>
2019-01-21 21:22:55 +0000

    add seed feature

--- seed.js
+++ seed.js
@@ -6,50 +6,51 @@ const uuid = require('uuid');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 module.exports.seedData = (evt, ctx, cb) => {
-  var stream = fs.createReadStream('./src/api/directory.csv');
-
-  var csvStream = csv().on('data', function(data) {
-    console.log(data[1]);
-    const params = {
-      TableName: 'shops',
-      Item: {
-        id: uuid.v1(),
-        brand: store[0],
-        storeNumber: store[1],
-        ownershipType: store[2],
-        country: store[3],
-        timezone: store[4],
-        phoneNumber: store[5],
-        storeName: store[6],
-        streetAddress: store[7],
-        city: store[8],
-        postCode: store[9],
-        latitude: store[10],
-        longitude: store[11]
-      }
-    };
-
-    dynamoDB
-      .put(params, (err, data) => {
-        if (err) {
-          console.error(err);
-          cb(new Error('COuld not fetch this store.'));
-          return;
-        } else {
-          cb(null, {
-            statusCode: 200,
-            headers: {
-              'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-              'Access-Control-Allow-Credentials': true // Required for cookies, authorization headers with HTTPS
-            },
-            body: JSON.stringify({ store: data.Item })
-          });
+  csv
+    .fromPath(csvFilePath, {
+      headers: true,
+      ignoreEmpty: true
+    })
+    .on('data', function(data) {
+      for (var value in data) {
+        if (data.hasOwnProperty(value)) {
+          if (data[value] === '') {
+            data[value] = 'N/A';
+          }
         }
-      })
-      .on('end', function() {
-        console.log('done');
-      });
 
-    stream.pipe(csvStream);
-  });
+        params = {
+          TableName: 'shops',
+          Item: {
+            id: uuid.v1(),
+            brand: data['Brand'] || "N/A",
+            storeNumber: data['Store Number'] || "N/A",
+            ownershipType: data['Ownership Type'] || "N/A",
+            country: data['Country'] || "N/A",
+            timezone: data['Timezone'] || "N/A",
+            phoneNumber: data['Phone Number'] || "N/A",
+            storeName: data['Store Name'] || "N/A",
+            streetAddress: data['Street Address'] || "N/A",
+            city: data['City'] || "N/A",
+            postCode: data['Postcode'] || "N/A",
+            latitude: data['Latitude'] || "N/A",
+            longitude: data['Longitude' || "N/A"]
+            //lots of other data
+          }
+        };
+        dynamoDB.put(params, function(err, data) {
+          if (err) {
+            console.error(
+              'Unable to add item. Error JSON:',
+              JSON.stringify(err, null, 2)
+            );
+          } else {
+            console.log('Added item:', JSON.stringify(data, null, 2));
+          }
+        });
+      }
+    })
+    .on('end', function() {
+      console.log('done');
+    });
 };
